5cdf855cef16a61cdb734d3503fef367b9221c78
MILBRU <brulez1234@gmail.com>
2026-02-02 22:15:26 +0100

    Initial commit

---
+++ src/main/java/org/milbru/mbtech/Mbtech.java
@@ -0,0 +1,10 @@
+package org.milbru.mbtech;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mbtech implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
